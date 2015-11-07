368881ef5a8344f3fa23d16ea34e5369a18a1ba1
acrobat130 <shanna.sullivan@gmail.com>
2015-11-06 17:02:53 -0800

    started adding angular code to /links. passing 9 tests.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,17 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+
+  $scope.getLinks = function(){
+    Links.getLinks($scope.data)
+      .then(function(data){
+        $scope.data.links = data;
+      })
+      .catch(function (error) {
+      });
+  };
+
+  $scope.getLinks();
+
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,7 +2,24 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getLinks = function(){
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    })
+    .then(function(resp) {
+      console.log('this is response.data', resp.data);
+      return resp.data;
+    })
+
+  };
+
+  return {
+    getLinks: getLinks
+  };
+
 })
+
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
